3ce7492b8c923b4277174e4ed05d0d614888590a
Tom Yu <chenzhentom951236@gmail.com>
2022-08-25 17:07:43 +0800

    Day 2 Controller 和 Minimal API 比較

---
+++ LineBotMessage/LineBotMessage/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LineBotMessage.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        public HelloWorldController()
+        {
+
+        }
+
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [HttpGet("Get")]
+        public string HelloWorld2()
+        {
+            return "Hello World 2";
+        }
+    }
+}
