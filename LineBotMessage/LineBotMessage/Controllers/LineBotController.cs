c3360245f2af394a6d6c5a6882f70941577ad091
chenzhen_Tom <chenzhenTom951236@gmail.com>
2022-08-29 12:49:09 +0800

    update git ignore

---
+++ LineBotMessage/LineBotMessage/Controllers/LineBotController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LineBotMessage.Controllers
+{
+
+    [Route("api/v{version:apiVersion}/[Controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class LineBotController : ControllerBase
+    {
+
+        // 貼上 messaging api channel 中的 accessToken & secret
+        private readonly string channelAccessToken = "Your channel access token";
+        private readonly string channelSecret = "Your channel secret";
+
+        // constructor
+        public LineBotController()
+        {
+
+        }
+
+        [HttpPost("Webhook")]
+        public IActionResult Webhook()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetWebhook")]
+        public IActionResult GetWebhook()
+        {
+            return Ok();
+        }
+    }
+}
--- LineBotMessage/LineBotMessage/Program.cs
+++ LineBotMessage/LineBotMessage/Program.cs
@@ -1,30 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+configuration.SetBasePath(environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
 // Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// add api version config
+builder.Services.AddApiVersioning(o => {
+    o.ReportApiVersions = true;
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+// add swagger config
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
-app.MapGet("minimalApi/HelloWorld", () =>
-{
-    return "Hello World";
-});
+app.UseAuthorization();
 
-app.MapGet("minimalApi/HelloWorld/Get", () =>
-{
-    return "Hello World 2";
-});
+app.MapControllers();
 
 app.Run();
+
