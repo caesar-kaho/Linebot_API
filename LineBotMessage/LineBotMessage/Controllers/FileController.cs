e6f773c4f09805520853003a5dda73a0c707d3c0
chenzhen_Tom <chenzhenTom951236@gmail.com>
2022-09-13 01:18:45 +0800

    File Controller

--- LineBotMessage/LineBotMessage/Controllers/FileController.cs
+++ LineBotMessage/LineBotMessage/Controllers/FileController.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
 namespace LineBotMessage.Controllers
 {
-    public class FileController
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
     {
-        public FileController()
+
+        private readonly IConfiguration _configuration;
+        public FileController(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Only for Imagemap message 讀取圖片
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <param name="imageSize"></param>
+        /// <returns></returns>
+        [HttpGet("UploadFiles/ImagemapImages/{folderName}/{imageSize}")]
+        public IActionResult GetFile(string folderName, string imageSize)
+        {
+            //FileStream fs = new FileStream();
+            var path = $"{_configuration.GetValue<string>(WebHostDefaults.ContentRootKey)}/UploadFiles/ImagemapImages/{folderName}/{imageSize}.png";
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                var fileBytes = System.IO.File.ReadAllBytes(path);
+                new FileExtensionContentTypeProvider().TryGetContentType(Path.GetFileName(path), out var contentType);
+                return new FileContentResult(fileBytes, contentType ?? "application/octet-stream");
+            }
         }
     }
 }
