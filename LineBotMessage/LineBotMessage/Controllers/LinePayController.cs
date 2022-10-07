a40861272274127acf8959430233ca60d31b31bd
chenzhen_Tom <chenzhenTom951236@gmail.com>
2022-10-08 04:17:42 +0800

    Line Pay 初步交易流程

---
+++ LineBotMessage/LineBotMessage/Controllers/LinePayController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LineBotMessage.Dtos;
+using LineBotMessage.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using LineBotMessage.Enum;
+using LineBotMessage.Providers;
+using System.Net.Mime;
+
+namespace LineBotMessage.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class LinePayController : ControllerBase
+    {
+        private readonly LinePayService _linePayService;
+        public LinePayController()
+        {
+            _linePayService = new LinePayService();
+        }
+
+        [HttpPost("Create")]
+        public async Task<PaymentResponseDto> CreatePayment(PaymentRequestDto dto)
+        {
+            return await _linePayService.SendPaymentRequest(dto);
+        }
+
+        [HttpPost("Confirm")]
+        public async Task<PaymentConfirmResponseDto> ConfirmPayment([FromQuery] string transactionId, [FromQuery] string orderId, PaymentConfirmDto dto )
+        {
+            return await _linePayService.ConfirmPayment(transactionId, orderId,dto);
+        }
+
+        [HttpGet("Cancel")]
+        public async void CancelTransaction([FromQuery] string transactionId)
+        {
+            _linePayService.TransactionCancel(transactionId);
+        }
+    }
+}
+
